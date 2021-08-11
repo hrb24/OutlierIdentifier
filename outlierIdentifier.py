ace773caa27b9429f5f9d79be9322b2ffd1c8648
hrb24 <hrb24@georgetown.edu>
2021-08-11 14:33:56 -0400

    Moved the test data description from the .txt file to the README.md file for simplicity. Also began implementing main() function

--- outlierIdentifier.py
+++ outlierIdentifier.py
@@ -1,1 +1,24 @@
 #This program identifies any outlier data points from a given input file
+
+def main():
+    # ------------------------------------------------------------- #
+    # The below block of code is for testing purposes only and      #
+    # should be commented out when the program is being used for a  #
+    # data set other than testData.txt                              #
+    # --------------------------------------------------------------#
+    
+    file = open("testData.txt","r")
+    
+    print("Line 1 is: ")
+    print(file.readline())
+    
+    print("Line 2 is: ")
+    print(file.readline())
+       
+    print("Line 3 is: ")
+    print(file.readline())
+    
+    file.close()
+    
+if (__name__ == "__main__"):
+    main()
